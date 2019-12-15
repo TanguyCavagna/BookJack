7642224ceec3ee1b197e760f4e391f6f4e3d7c76
Nocxxecru <nicolas.hr@eduge.ch>
2019-12-15 23:12:28 +0100

    login

--- public/js/app.js
+++ public/js/app.js
@@ -43,6 +43,39 @@ function login(event) {
 
     // processing
     if (username.length == 0) {
-        
+
+        $("#username").css("border-color", "red");
+        return;
+    } else {
+        $("#username").css("border-color", "");
     }
+
+    if (password.length == 0) {
+
+        $("#password").css("border-color", "red");
+        return;
+    } else {
+        $("#password").css("border-color", "");
+    }
+
+    $.ajax({
+        method: "post",
+        url: "../../App/Api/login.php",
+        data: { "username": username, "password": password },
+        dataType: "json",
+
+        success: (data) => {
+            switch (data.ReturnCode) {
+                case 0: // no error
+                    window.location = "../index.php"
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;    
+            }
+        }
+    });
 }
