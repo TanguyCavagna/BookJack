b44d399ffe3ac66bbd796721a3802ab33b263d24
Nocxxecru <nicohoarau74@gmail.com>
2019-12-16 20:19:00 +0100

    Page HTML register

---
+++ public/register.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Inscription</title>
+</head>
+<body>
+    <?php require_once './includes/nav.php'; ?>
+
+    <div class="mx-auto w-6/12 max-w-xs mt-24">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Username</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username">
+                <p class="hidden text-red-500 text-xs italic error-msg">This username is already taken.</p>
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="mail">Email</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="mail" name="mail" type="email" placeholder="abc@efg.xyz">
+                <p class="hidden text-red-500 text-xs italic error-msg">This email is already used.</p>
+            </div>
+            <div class="mb-8">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="******************">
+            </div>
+            <div class="mb-10">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="verify-password">Verify password</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="verify-password" name="verify-password" type="password" placeholder="******************">
+                <p class="hidden text-red-500 text-xs italic error-msg">This password is not the same as the one above.</p>
+            </div>
+            <div class="mb-12">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="profile-picture">Profile picture</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="profile-picture" name="profile-picture" type="file">
+            </div>
+            <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="submit" id="login">
+                    Sign In
+                </button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
